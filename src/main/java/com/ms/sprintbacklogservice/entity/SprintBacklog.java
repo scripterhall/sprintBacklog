dee0c53eb758cdff2400c390bbd1694c0b1483dc
nourallah <halloul.nourallah@gmail.com>
2023-06-02 00:26:39 +0100

    no extends

--- src/main/java/com/ms/sprintbacklogservice/entity/SprintBacklog.java
+++ src/main/java/com/ms/sprintbacklogservice/entity/SprintBacklog.java
@@ -1,10 +1,6 @@
 package com.ms.sprintbacklogservice.entity;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-import com.ms.sprintbacklogservice.model.Backlog;
 import com.ms.sprintbacklogservice.model.Sprint;
-
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -12,7 +8,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Transient;
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.EqualsAndHashCode;
+
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
@@ -21,16 +17,11 @@ import lombok.ToString;
 @AllArgsConstructor
 @NoArgsConstructor
 @ToString
-@EqualsAndHashCode(callSuper=false)
-public class SprintBacklog extends Backlog{
+public class SprintBacklog {
     
     @Id
     @GeneratedValue(strategy=GenerationType.IDENTITY)
     private Long id ; 
-
-    @Column(name="nbr_heurs")
-    private int nbrHeurs; 
-
    
     private Long sprintId;
 
--- src/main/java/com/ms/sprintbacklogservice/model/ProductBacklog.java
+++ src/main/java/com/ms/sprintbacklogservice/model/ProductBacklog.java
@@ -4,18 +4,18 @@ import java.util.Date;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.EqualsAndHashCode;
+
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 @Data
-@EqualsAndHashCode(callSuper=false)
+
 @ToString
 @NoArgsConstructor
 @AllArgsConstructor
-public class ProductBacklog extends Backlog{
+public class ProductBacklog {
 
     private Long id; //L'identifiant du product backlog
     private Date dateCreation; //La date de création du product backlog
-    
+    private int velocite ;
 }
